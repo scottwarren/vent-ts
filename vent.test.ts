5473cd15ef09e387e0f2e44dca5176db965457b6
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2023-09-13 17:32:59 +0300

    chore: fixes tests post-refactor

--- vent.test.ts
+++ vent.test.ts
@@ -1,41 +1,45 @@
-import { vent } from "./vent";
+import { Vent } from './vent'
 
-describe("vent tests", () => {
-  test("events that are subscribed to get run", () => {
-    const cb = jest.fn();
+describe('vent tests', () => {
+  test('events that are subscribed to get run', () => {
+    const vent = new Vent()
+    const cb = jest.fn()
 
-    const exampleEvent = vent.on("test", cb);
-    expect(cb).toBeCalledTimes(0);
-    vent.trigger("test");
-    expect(cb).toBeCalledTimes(1);
-  });
+    const exampleEvent = vent.on('test', cb)
+    expect(cb).toBeCalledTimes(0)
+    vent.trigger('test')
+    expect(cb).toBeCalledTimes(1)
+  })
 
-  test("events should unsubscribe", () => {
-    const cb = jest.fn();
+  test('events should unsubscribe', () => {
+    const vent = new Vent()
+    const cb = jest.fn()
 
-    const exampleEvent = vent.on("test", cb);
+    const exampleEvent = vent.on('test', cb)
 
-    expect(cb).toBeCalledTimes(0);
-    vent.off(exampleEvent);
-    expect(cb).toBeCalledTimes(0);
-    vent.trigger("test");
-    expect(cb).toBeCalledTimes(0);
-  });
+    expect(cb).toBeCalledTimes(0)
+    vent.off(exampleEvent)
+    expect(cb).toBeCalledTimes(0)
+    vent.trigger('test')
+    expect(cb).toBeCalledTimes(0)
+  })
 
-  test("events that are subscribed to with multiple callbacks", () => {
-    const cb1 = jest.fn();
-    const cb2 = jest.fn();
+  test('events that are subscribed to with multiple callbacks', () => {
+    const vent = new Vent()
+    const cb1 = jest.fn()
+    const cb2 = jest.fn()
 
-    vent.on("test", cb1);
-    vent.on("test", cb2);
-    expect(cb1).toBeCalledTimes(0);
-    expect(cb2).toBeCalledTimes(0);
-    vent.trigger("test");
-    expect(cb1).toBeCalledTimes(1);
-    expect(cb2).toBeCalledTimes(1);
-  });
+    vent.on('test', cb1)
+    vent.on('test', cb2)
+    expect(cb1).toBeCalledTimes(0)
+    expect(cb2).toBeCalledTimes(0)
+    vent.trigger('test')
+    expect(cb1).toBeCalledTimes(1)
+    expect(cb2).toBeCalledTimes(1)
+  })
 
-  test("error is thrown when event does not exist", () => {
-    expect(() => vent.trigger("random name")).toThrowError();
-  });
-});
+  test('should execute without error if an event is triggered that has no callbacks', () => {
+    const vent = new Vent()
+    expect(() => vent.trigger('random name'))
+  })
+})
